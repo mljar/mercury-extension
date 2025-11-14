84d76fd607a0e5d8760e686ea1fff88ffdf9f7f4
Piotr <pplonski86@gmail.com>
2025-11-14 11:56:03 +0100

    easier import

--- mercury/__init__.py
+++ mercury/__init__.py
@@ -4,6 +4,7 @@ from .button import Button
 from .md import Markdown
 from .multiselect import MultiSelect
 from .indicator import Indicator
+from .table import Table
 from .chat.chat import Chat
 from .chat.chatinput import ChatInput
 from .chat.message import Message 
